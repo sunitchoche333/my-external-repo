6bc03ac8695009e7cd37c4e9293e49d16f66ed70
Sunit <136069249+sunitchoche333@users.noreply.github.com>
2025-03-27 12:44:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-import { getGridSingleSelectOperators } from '@mui/x-data-grid-premium';
 import GroupLabel from './components/GroupLabel';
-
-const CustomMultiFilterOperators = (filters) => {
-    const singleSelectOperators = getGridSingleSelectOperators();
-
-    const filterOperators = filters.map(filter => {
-        const { label, getApplyFilterFn } = filter;
-        return {
-            label,
-            value: label,
-            getApplyFilterFn,
-            InputComponent: singleSelectOperators?.[2]?.InputComponent,
-        };
-    });
-
-    return { filterOperators };
-}
+import CustomMultiFilterOperators from './CustomMultiFilterOperators';
 
 export { CustomMultiFilterOperators, GroupLabel };
